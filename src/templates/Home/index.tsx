11b6df1be4fda12057b1c987bd8cae91d455fe72
Carlos Daniel <carlosdandeveloper@gmail.com>
2024-06-11 19:18:26 -0300

    Criando o template Base via TDD

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,11 +1,10 @@
+import Base from 'templates/Base'
+
 import { BannerProps } from 'components/Banner'
 import { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
-
 import { Container } from 'components/Container'
-import Menu from 'components/Menu'
 import BannerSlider from 'components/BannerSlider'
-import Footer from 'components/Footer'
 import Showcase from 'components/Showcase'
 
 import * as S from './styles'
@@ -33,10 +32,8 @@ const Home = ({
   freeGames,
   freeHighlight
 }: HomeTemplateProps) => (
-  <section>
+  <Base>
     <Container>
-      <Menu />
-
       <S.SectionBanner>
         <BannerSlider items={banners} />
       </S.SectionBanner>
@@ -58,13 +55,7 @@ const Home = ({
     </S.SectionUpcoming>
 
     <Showcase title="Free games" highlight={freeHighlight} games={freeGames} />
-
-    <S.SectionFooter>
-      <Container>
-        <Footer />
-      </Container>
-    </S.SectionFooter>
-  </section>
+  </Base>
 )
 
 export default Home

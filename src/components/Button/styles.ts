08dbaef0f38c6773c7aae6e97f7c357fffac9209
Carlos Daniel <carlosdandeveloper@gmail.com>
2024-07-22 11:55:41 -0300

    Criando componente PaymentOptions

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -12,23 +12,19 @@ const wrapperModifiers = {
     height: 3rem;
     font-size: ${theme.font.sizes.xsmall};
   `,
-
   medium: (theme: DefaultTheme) => css`
     height: 4rem;
     font-size: ${theme.font.sizes.small};
     padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
   `,
-
   large: (theme: DefaultTheme) => css`
     height: 5rem;
     font-size: ${theme.font.sizes.medium};
     padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
   `,
-
   fullWidth: () => css`
     width: 100%;
   `,
-
   withIcon: (theme: DefaultTheme) => css`
     svg {
       width: 1.5rem;
@@ -38,7 +34,6 @@ const wrapperModifiers = {
       }
     }
   `,
-
   minimal: (theme: DefaultTheme) => css`
     background: none;
     color: ${theme.colors.primary};
@@ -46,11 +41,17 @@ const wrapperModifiers = {
     &:hover {
       color: ${darken(0.1, theme.colors.primary)};
     }
+  `,
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
+    }
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal, disabled }) => css`
     display: inline-flex;
     align-items: center;
     justify-content: center;
@@ -72,5 +73,6 @@ export const Wrapper = styled.button<WrapperProps>`
     ${!!fullWidth && wrapperModifiers.fullWidth}
     ${hasIcon && wrapperModifiers.withIcon(theme)}
     ${!!minimal && wrapperModifiers.minimal(theme)}
+    ${disabled && wrapperModifiers.disabled()}
   `}
 `

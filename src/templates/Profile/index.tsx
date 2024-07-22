36c2a9fb6a4017bf4d521bdce15278903f0cb968
Carlos Daniel <carlosdandeveloper@gmail.com>
2024-07-22 16:45:40 -0300

    Criando as páginas /profile e /cards

---
+++ src/templates/Profile/index.tsx
@@ -0,0 +1,27 @@
+import { Container } from 'components/Container'
+import Base from 'templates/Base'
+import Heading from 'components/Heading'
+import ProfileMenu from 'components/ProfileMenu'
+
+import * as S from './styles'
+
+export type ProfileTemplateProps = {
+  children: React.ReactNode
+}
+
+const Profile = ({ children }: ProfileTemplateProps) => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        My profile
+      </Heading>
+
+      <S.Main>
+        <ProfileMenu />
+        <S.Content>{children}</S.Content>
+      </S.Main>
+    </Container>
+  </Base>
+)
+
+export default Profile
